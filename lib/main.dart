c32e656ebdfaeb811f82c9a738920afa9eae09a0
tch1001 <tanchienhao@gmail.com>
2022-11-30 11:55:40 +0800

    working absolute positioning!

--- lib/main.dart
+++ lib/main.dart
@@ -63,36 +63,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Stack(
+      children: [
+        Image.asset('assets/homepage.png'),
+        Positioned(
+          left: 40,
+          top: 20,
+          child: ElevatedButton(
+            onPressed: (){
+              print("meow");
+            },
+            child: Container(
+              height: 50,
+              width: 150,
+              child: Icon(Icons.add),
+            ),
+          )
+        )
+      ]
+    );
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       body: SingleChildScrollView(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('assets/homepage.png'),
@@ -104,15 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Container(
                       height: 150,
                       width: 50,
-                      decoration: BoxDecoration(
-                          // borderRadius: BorderRadius.circular(1),
-                          // color: Colors.black
-                          ),
                       child: Icon(Icons.add)),
 
               ),
             )
-            // ButtonBar(children: [Icon(Icons.add), Icon(Icons.add)],)
           ],
         ),
       ),
