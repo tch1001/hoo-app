95727095b6ae15a833122c5a09b851c082c6f774
tch1001 <tanchienhao@gmail.com>
2022-12-01 09:30:27 +0800

    water usage done

--- lib/WaterUsageScreen.dart
+++ lib/WaterUsageScreen.dart
@@ -20,6 +20,53 @@ class _WaterUsageScreenState extends State<WaterUsageScreen>{
             Image.asset('assets/watersmile.png', height: 150),
             createRichButton("assets/watericon.png", "XXX litres", () { }),
             createRichButton("assets/clock.png", "XXX mins", () { print("clock"); }),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () { print("hi"); },
+                style: buttonStyle,
+                child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("INDIVIDUAL USAGE", style: TextStyle(
+                              color: mydarkblue,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          )),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(child: Image.asset("assets/completion.png", height: 100,), padding: EdgeInsets.all(10.0),),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Padding(
+                                        child: Text("TAP 1: Kitchen", style: buttonTextStyle),
+                                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0)
+                                    )
+                                ),
+                                Padding(
+                                    child: Text("XX litres of water used", style: buttonTextStyle),
+                                    padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0)
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ]
+                ),
+              ),
+            )
           ],
         )
     );
